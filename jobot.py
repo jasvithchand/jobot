d5da2008a5f132ccddb0dcdf9473cde216eab081
Jasvith Chand Anne <a.jasvithchand@gmail.com>
2024-09-02 01:59:15 -0700

    Final Page Complete

--- jobot.py
+++ jobot.py
@@ -8,6 +8,53 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementClickInterceptedException
 import time
+import os
+
+def handle_voluntary_disclosures(driver):
+
+    #Handle Gender
+    # Open the gender dropdown
+    gender_dropdown = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, xpaths['genderDropdown']))
+    )
+    gender_dropdown.click()
+    print("Gender dropdown clicked")
+
+    time.sleep(1)
+
+    # Wait for the gender option to be visible
+    gender_to_select = user_data['gender']  # Make sure 'State' is correctly named in userinfo.json
+    gender_option_xpath = xpaths['genderOption'].replace('{gender}', gender_to_select)
+    gender_option = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, gender_option_xpath))
+    )
+
+    time.sleep(1)
+
+    # Click the gender option
+    driver.execute_script("arguments[0].scrollIntoView();", gender_option)
+    gender_option.click()
+    print(f"Selected gender: {gender_to_select}")
+    time.sleep(1)
+
+
+    # Check "I agree to the terms and conditions"
+    try:
+        # Wait for any known obstructions to disappear
+        driver.switch_to.active_element
+        checkbox = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, xpaths['agreementCheckbox']))
+        )
+        driver.find_element(By.XPATH, xpaths['agreementCheckbox']).click()
+        print("Clicked 'I agree to the terms and conditions' checkbox.")
+    except (NoSuchElementException, TimeoutException):
+        print("Agreement checkbox not found.")
+        pass
+
+    time.sleep(1)
+
+
+
 
 #Function to handle My experience Page
 def handle_my_experience_step(driver):
@@ -19,11 +66,12 @@ def handle_my_experience_step(driver):
             EC.element_to_be_clickable((By.XPATH, xpaths['addExperienceButton']))
         )
         add_experience_button.click()
-        print("Clicked 'Save and Continue' button.")
-    except NoSuchElementException:
-        pass
+        print("Clicked 'Add' button.")
+    except Exception as e:
+        print(f"An error occurred while clicking 'Add' button: {e}")
 
-    time.sleep(2)
+    time.sleep(5)
+
     #Click Add Another Work Experience
     try:
         button = WebDriverWait(driver, 10).until(
@@ -93,7 +141,7 @@ def handle_my_experience_step(driver):
                     role_description_input.send_keys(user_data[description_key])
                     print(f"Entered {user_data[description_key]} for Role Description {index}.")
             
-            for i in range(0, len(from_months)):
+            for i in range(0, len(from_months)-1):
                 index = i // 2 + 1  # Determine which work experience entry we are dealing with based on the index
                 month_key = f"FromMonth{index:02}"
                 year_key = f"FromYear{index:02}"
@@ -122,47 +170,144 @@ def handle_my_experience_step(driver):
 
     enter_work_experience_details(driver)
 
+    # Click elsewhere on the page to lose focus
+    body = driver.find_element(By.TAG_NAME, 'body')
+    body.click()
+    print("Clicked elsewhere to lose focus")
+
+    driver.switch_to.default_content()
+
     # Check "I currently work here"
     try:
         # Wait for any known obstructions to disappear
-        WebDriverWait(driver, 20).until(
-            EC.invisibility_of_element((By.CSS_SELECTOR, "div.some-overlay"))
+        driver.switch_to.active_element
+        checkbox = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, xpaths['currentWorkCheckbox']))
         )
-        # Now try clicking the checkbox
-        checkbox = driver.find_element(By.ID, "input-22")
-        driver.execute_script("arguments[0].click();", checkbox)
-    except NoSuchElementException:
+        driver.find_element(By.XPATH, xpaths['currentWorkCheckbox']).click()
+        print("Clicked 'I currently work here' checkbox.")
+    except (NoSuchElementException, TimeoutException):
         print("Current work checkbox not found.")
         pass
 
-
-    # Enter "To Date Month"
+    #Enter School Name
     try:
-        toDateMonth02 = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, xpaths['inputCompanytoMonth02']))
+        school_name_field = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, xpaths['inputEducationSchoolName']))
         )
-        toDateMonth02.clear()
-        time.sleep(1)
-        toDateMonth02.send_keys(user_data['toMonth02'])
-        print("Entered toMonth02.")
-    except NoSuchElementException:
-        print("From Date field not found.")
-        pass
+        school_name_field.clear()
+        school_name_field.send_keys(user_data['school_name'])
+        print("Entered School Name.")
+    except Exception as e:
+        print(f"An error occurred while entering school name: {e}")
+
+    #Handle Degree
+    # Open the degree dropdown
+    degree_dropdown = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, xpaths['degreeDropdown']))
+    )
+    degree_dropdown.click()
+    print("Degree dropdown clicked")
+
+    time.sleep(1)
+
+    # Wait for the degree option to be visible
+    degree_to_select = user_data['degree']  # Make sure 'degree' is correctly named in userinfo.json
+    degree_option_xpath = xpaths['degreeOption'].replace('{degree}', degree_to_select)
+    degree_option = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, degree_option_xpath))
+    )
+
+    time.sleep(1)
+
+    # Click the degree option
+    driver.execute_script("arguments[0].scrollIntoView();", degree_option)
+    degree_option.click()
+    print(f"Selected degree: {degree_to_select}")
+    time.sleep(1)
+
+
+    # #Add Education
+    # try:
+    #     add_education_button = WebDriverWait(driver, 10).until(
+    #         EC.element_to_be_clickable((By.XPATH, xpaths['addEducationButton']))
+    #     )
+    #     add_education_button.click()
+    #     print("Clicked 'Add Education' button.")
+    # except Exception as e:
+    #     print(f"An error occurred while adding education: {e}")
+
+    # #Enter Skills
+    # try:
+    #     skills_field = WebDriverWait(driver, 10).until(
+    #         EC.element_to_be_clickable((By.XPATH, xpaths['inputSkills']))
+    #     )
+    #     skills_field.clear()
+    #     skills_field.send_keys(user_data['skills'])
+    #     print("Entered Skills.")
+    # except Exception as e:
+    #     print(f"An error occurred while entering skills: {e}")
+    #     pass
     
-    # Enter "To Date Month"
+    #upload resume
+    delete_resumes = driver.find_elements(By.CSS_SELECTOR, "button[data-automation-id='delete-file']")
+    i = 1
+    while i <= len(delete_resumes):
+      driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='delete-file']").click()
+      i = i+1
+      time.sleep(1)
+    clear
+    file_input = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
+    file_input.clear()
+    file_input.send_keys(os.path.abspath(user_data['resume_path']))
+    time.sleep(5)
+
+    #Add Websites
     try:
-        toDateMonth02 = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, xpaths['inputCompanytoMonth02']))
+        add_websites_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, xpaths['addWebsitesButton']))
         )
-        toDateMonth02.clear()
+        add_websites_button.click()
+        print("Clicked 'Add Websites' button.")
+        time.sleep(0.5)
+        input_website_url = driver.find_element(By.XPATH, xpaths['inputWebsiteUrl'])
+        input_website_url.clear()
+        input_website_url.send_keys(user_data['website_url'])
+        print("Entered Website URL.")
+
         time.sleep(1)
-        toDateMonth02.send_keys(user_data['toMonth02'])
-        print("Entered toMonth02.")
-    except NoSuchElementException:
-        print("From Date field not found.")
-        pass
+
+    except Exception as e:
+        print(f"An error occurred while adding websites: {e}")
+    
+    #Add LinkedIn
+    try:
+      linkedin_question = driver.find_element(By.CSS_SELECTOR, "input[type='text'][data-automation-id='linkedinQuestion']")
+      linkedin_question.clear()
+      linkedin_question.send_keys(user_data['linkedin_url'])
+      print("Entered LinkedIn URL.")
+    except:
+      print("Exception: 'No Linkedin input'")
 
 
+def click_next(driver):
+    try:
+        # Click the 'Next' button
+        button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-automation-id='bottom-navigation-next-button']")))
+        button.click()
+
+        # Check for errors after clicking the button
+        try:
+            error_button = driver.find_element(By.CSS_SELECTOR, "button[data-automation-id='errorBanner']")
+            print("Exception: 'Errors on page. Please resolve and submit manually. You have 60 seconds to do so!'")
+            time.sleep(60)  # Wait to manually resolve errors
+        except NoSuchElementException:
+            print("No Errors")
+
+        # Wait to ensure any post-click actions complete
+        time.sleep(3)
+    except Exception as e:
+        print(f"An error occurred while clicking 'Next' button: {e}")   
 
 # Function to handle actions specific to "My Information" step
 def handle_how_did_you_hear(driver):
@@ -202,11 +347,12 @@ def handle_my_information_step(driver):
         # Handle 'How Did You Hear About Us?' field
         handle_how_did_you_hear(driver)
 
-        # previously_worked_no = WebDriverWait(driver, 10).until(
-        #     EC.element_to_be_clickable((By.XPATH, '//*[@id="2"]'))
-        # )
-        # previously_worked_no.click()
-        # print("Clicked 'Previously worked' No option")
+        try:
+            driver.switch_to.active_element
+            driver.find_element(By.CSS_SELECTOR, "input[type='radio'][data-uxi-element-id='radio_2']").click()
+            print("Clicked 'Previously worked' No option - 2")
+        except:
+            print("Exception: 'No radio_2 button'")
 
         time.sleep(1)
 
@@ -216,6 +362,8 @@ def handle_my_information_step(driver):
         )
         dropdown.click()
         print("Clicked country dropdown")
+
+        time.sleep(0.5)
 
         # Wait for the dropdown options to be visible
         WebDriverWait(driver, 10).until(
@@ -229,24 +377,27 @@ def handle_my_information_step(driver):
         print("Selected 'United States of America' from dropdown")
 
         # Handle FirstName
-        first_name_field = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, xpaths['inputFirstName']))
-        )
-        first_name_field.clear()
-        first_name_field.send_keys(user_data['FirstName'])
-        print("Entered FirstName")
+        try:
+            first_name_field = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, xpaths['inputFirstName']))
+            )
+            first_name_field.clear()
+            first_name_field.send_keys(user_data['FirstName'])
+            print("Entered FirstName")
+        except Exception as e:
+            print(f"An error occurred filling first name: {e}")
 
-        time.sleep(1)
+
 
         # Handle LastName
-        last_name_field = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, xpaths['inputLastName']))
-        )
-        last_name_field.clear()
-        last_name_field.send_keys(user_data['LastName'])
-        print("Entered LastName")
+        try:
+            driver.find_element(By.CSS_SELECTOR, "input[type='text'][data-automation-id='legalNameSection_lastName']").clear()
+            time.sleep(1)
+            driver.find_element(By.CSS_SELECTOR, "input[type='text'][data-automation-id='legalNameSection_lastName']").send_keys(user_data['LastName'])
+            print("Entered LastName")
+        except Exception as e:
+            print(f"An error occurred filling last name: {e}")
 
-        time.sleep(1)
 
         # Handle Address Line 1
         address_line_1_field = WebDriverWait(driver, 10).until(
@@ -331,6 +482,7 @@ def handle_my_information_step(driver):
         save_and_continue_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, xpaths['myInformationSubmit']))
         )
+        driver.execute_script("arguments[0].scrollIntoView();", save_and_continue_button)
         save_and_continue_button.click()
         print("Clicked 'Save and Continue' button.")
 
@@ -339,8 +491,6 @@ def handle_my_information_step(driver):
             EC.visibility_of_element_located((By.XPATH, '//*[contains(text(), "My Experience")]'))
         )
         print("Navigated to 'My Experience' step.")
-
-        handle_my_experience_step(driver)
 
 
     except TimeoutException as e:
@@ -393,6 +543,8 @@ service = Service(executable_path=driver_path)
 # Initialize the WebDriver
 driver = webdriver.Edge(service=service)
 
+actions = ActionChains(driver)
+
 # Navigate to the login page using the URL from the URLs file
 driver.get(urls['loginUrl'])
 
@@ -402,12 +554,12 @@ try:
     email_input.send_keys(user_data['email'])
     password_input = driver.find_element(By.XPATH, xpaths['passwordInput'])
     password_input.send_keys(user_data['password'])
-    time.sleep(1)
+    time.sleep(0.5)
 
     # Click the Sign-In Button
     sign_in_button = driver.find_element(By.XPATH, xpaths['signInButton'])
     ActionChains(driver).move_to_element(sign_in_button).click().perform()
-    time.sleep(1)
+    time.sleep(0.5)
 
     # Wait for the page to load after login
     WebDriverWait(driver, 20).until(
@@ -422,6 +574,27 @@ try:
 
     if "My Information" in current_step:
         handle_my_information_step(driver)
+        click_next(driver)
+        current_step = driver.find_element(By.XPATH, '//*[@id="mainContent"]/div/div[3]/h2')
+        current_step = current_step_header.text
+        print(f"Currently at: {current_step}")
+        handle_my_experience_step(driver)
+        click_next(driver)
+        current_step = driver.find_element(By.XPATH, '//*[@id="mainContent"]/div/div[3]/h2')
+        current_step = current_step_header.text
+        print(f"Currently at: {current_step}")
+        handle_voluntary_disclosures(driver)
+        click_next(driver)
+        current_step = driver.find_element(By.XPATH, '//*[@id="mainContent"]/div/div[3]/h2')
+        current_step = current_step_header.text
+        print(f"Currently at: {current_step}")
+
+        #Final Submit
+        click_next(driver)
+        time.sleep(10)
+
+        #driver.quit()
+
     else:
         print(f"Unexpected step: {current_step}")
 
